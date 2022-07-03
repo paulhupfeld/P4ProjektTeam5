05e1dca2c5f6c526ec1846164cf8ea48d124f5d3
paulhupfeld <paul.hupfeld@stud.h-da.de>
2022-07-03 15:46:12 +0200

    ws

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -2,6 +2,24 @@ import Navigator from "./Navigator.js";
 
 export let navigator;
 
+// Create WebSocket connection.
+const socket = new WebSocket("ws://localhost:3000");
+
+// Connection opened
+socket.addEventListener("open", function (event) {
+  console.log("Connected to WS Server");
+});
+
+// Listen for messages
+socket.addEventListener("message", function (event) {
+  console.log("Message from server", event.data);
+});
+
+// Send a message
+const sendMessage = () => {
+  socket.send("Hello From Client1!");
+};
+
 function draw() {
   navigator = new Navigator();
 
