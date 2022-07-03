c46939156e5401f5e4f5bb6d381d06280bda8a08
paulhupfeld <paul.hupfeld@stud.h-da.de>
2022-07-03 16:15:10 +0200

    ws into Navigator

--- javascript/navigator.js
+++ javascript/navigator.js
@@ -1,7 +1,8 @@
 import { displayLevel } from "../p5setup.js";
 
 export default class Navigator {
-  constructor() {
+  constructor(socket) {
+    this.socket = socket;
     this.setUpLevel = true;
     this.currentLevel = 1;
     this.commands = ["vorwärts", "rechtsDrehen", "vorwärts", "aufsammeln"];
@@ -9,13 +10,29 @@ export default class Navigator {
     this.levelIsDone = false;
   }
 
-  setUpLevel() {
-    if (this.setUpLevel === true) {
-      //display level
-    }
+  wsCommunicator() {
+    // Connection opened
+    this.socket.addEventListener("open", function (event) {
+      this.send("Hello From Client1!");
+      console.log("Connected to WS Server");
+    });
+
+    // Listen for messages
+    this.socket.addEventListener("message", function (event) {
+      console.log("Received message =>", event.data);
+    });
+
+    // // Send a message
+    // const sendMessage = () => {
+    //   this.send("Hello From Client1!");
+    // };
   }
 
-  wsCommunicator() {}
+  setUpLevel() {
+    if (this.setUpLevel === true) {
+      //...
+    }
+  }
 
   translateIDIntoCommand() {}
 
--- javascript/sketch.js
+++ javascript/sketch.js
@@ -5,25 +5,10 @@ export let navigator;
 // Create WebSocket connection.
 const socket = new WebSocket("ws://localhost:3000");
 
-// Connection opened
-socket.addEventListener("open", function (event) {
-  console.log("Connected to WS Server");
-});
-
-// Listen for messages
-socket.addEventListener("message", function (event) {
-  console.log("Received message =>", event.data);
-  socket.send("Hello From Client1!");
-});
-
-// // Send a message
-// const sendMessage = () => {
-//   socket.send("Hello From Client1!");
-// };
+navigator = new Navigator(socket);
 
 function draw() {
-  navigator = new Navigator();
-
+  navigator.wsCommunicator();
   navigator.display();
 }
 
