5e7e5731891265fe1b0939fbabe3ad1b6e104684
paulhupfeld <paul.hupfeld@stud.h-da.de>
2022-07-19 18:22:38 +0200

    structured index.js

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
-//Webserver:
+//Initialize serialport:
+const SerialPort = require("serialport");
+const Readline = SerialPort.parsers.Readline;
+
+const port = new SerialPort(
+  "/dev/tty.usbmodem11101",
+  { baudRate: 9600 },
+  function (err) {
+    if (err) {
+      return console.log("Error: ", err.message);
+    }
+  }
+);
+
+const parser = port.pipe(new Readline({ delimiter: "\n" }));
+
+//Initialize webserver:
 const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ server: server });
 
+//Reveive array mit ids from Arduino (via serialport):
+port.on("open", () => {
+  console.log("serial port open");
+
+  parser.on("data", (data) => {
+    console.log("message from arduino:", data);
+  });
+
+  // port.write("SYST:ADDR?\n", function (err) {
+  //   if (err) {
+  //     return console.log("Error on write: ", err.message);
+  //   }
+  // });
+});
+
+//Send Array with command-objects and receive object with enlightenment info to/from webClient (via websocket):
 let startMessage = JSON.stringify({
   message: "start",
   commands: [
@@ -33,44 +65,11 @@ wss.on("connection", (ws) => {
 
 server.listen(3000, () => console.log(`Listening on port: 3000`));
 
-//---
-
-//Serialport:
-const SerialPort = require("serialport");
-const Readline = SerialPort.parsers.Readline;
-
-const port = new SerialPort(
-  "/dev/tty.usbmodem11101",
-  { baudRate: 9600 },
-  function (err) {
-    if (err) {
-      return console.log("Error: ", err.message);
-    }
-  }
-);
-
-const parser = port.pipe(new Readline({ delimiter: "\n" }));
-
-port.on("open", () => {
-  console.log("serial port open");
-
-  parser.on("data", (data) => {
-    console.log("message from arduino:", data);
-  });
-
-  // port.write("SYST:ADDR?\n", function (err) {
-  //   if (err) {
-  //     return console.log("Error on write: ", err.message);
-  //   }
-  // });
-});
-
+//Send objekt with enlightenment info to Arduino (via serialport):
 setTimeout(function () {
   port.write("<enlight, 1011, 1>");
   console.log("hi");
 }, 10000);
-
-//---
 
 //bei Start-Signal von Start-Button: setzte let start = true
 //sobald start === true: frage IDs ab + setze start = false
